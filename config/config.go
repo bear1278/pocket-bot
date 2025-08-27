440261fb6cdabb08edb5b6e1df3a16d345743499
bear1278 <grigorijanusik@gmail.com>
2025-08-27 23:19:00 +0300

    add new config

--- config/config.go
+++ config/config.go
@@ -8,12 +8,26 @@ type Config struct {
 
 func Init() (*Config, error) {
 	var config Config
-	viper.AddConfigPath("config")
-	viper.SetConfigName("config")
-	err := viper.ReadInConfig()
-	if err != nil {
+	//viper.AddConfigPath("config")
+	//viper.SetConfigName("config")
+	//err := viper.ReadInConfig()
+	//if err != nil {
+	//	return nil, err
+	//}
+	//config.Token = viper.GetString("token")
+	if err := ParseEnv(&config); err != nil {
 		return nil, err
 	}
-	config.Token = viper.GetString("token")
 	return &config, nil
+}
+
+func ParseEnv(cfg *Config) error {
+
+	if err := viper.BindEnv("token"); err != nil {
+		return err
+	}
+
+	cfg.Token = viper.GetString("token")
+	return nil
+
 }
